ff52e7c36e202a432da855545b227a06a4b547ee
Morten Sørensen <morten@moso.io>
2017-03-29 14:38:45 +0200

    Added moso-timeline component

---
+++ src/js/components/moso-timeline/moso-timeline.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul class="timeline">
+        <li>
+            <h3 class="title">{{ entry }}</h3>
+            <span class="time">{{ time }}</span>
+            <slot></slot>
+        </li>
+    </ul>
+</template>
+<script>
+    export default {
+        props: {
+            entry: {
+                type: String
+            },
+
+            time: {
+                type: String
+            }
+        },
+
+        mounted() {
+            //
+        }
+    }
+</script>
