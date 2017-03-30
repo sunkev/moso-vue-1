2854bfe89cf682856efcd9e876d67f3aa4b3be31
Morten Sørensen <morten@moso.io>
2017-03-30 12:55:16 +0200

    Added moso-profile and moso-profile-item components

---
+++ src/js/components/moso-profile/moso-profile-item.vue
@@ -0,0 +1,25 @@
+<template>
+    <li>
+        <a :href="link" tabindex="-1">
+            <i class="mdi" :class="icon"></i>
+            <nano-ripple></nano-ripple>
+        </a>
+    </li>
+</template>
+<script>
+    export default {
+        props: {
+            link: {
+                type: String
+            },
+
+            icon: {
+                type: String
+            }
+        },
+
+        mounted() {
+            //
+        }
+    }
+</script>
---
+++ src/js/components/moso-profile/moso-profile.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="profile">
+        <div class="profile-image">
+            <img :src="image">
+        </div>
+        <h4>{{ name }}</h4>
+        <h6>{{ position }}</h6>
+        <div class="icon-group">
+            <ul>
+                <slot></slot>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            image: {
+                type: String
+            },
+
+            name: {
+                type: String
+            },
+
+            position: {
+                type: String
+            }
+        },
+
+        mounted() {
+            //
+        }
+    }
+</script>
