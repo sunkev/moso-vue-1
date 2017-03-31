b64f675689608070d429e5f65157b015e41d63f6
Morten Sørensen <morten@moso.io>
2017-03-31 21:58:37 +0200

    Added moso-card-cover vue component

---
+++ src/js/components/moso-card/moso-card-cover.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="card">
+        <div class="card-img-cover">
+            <div class="card-img">
+                <img :src="image">
+            </div>
+        </div>
+        <div class="card-img-cover-content">
+            <div class="card-header">
+                <div class="card-title">{{ cardTitle }}</div>
+                <a class="card-subtitle" :href="link">{{ link }}</a>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            image: {
+                type: String
+            },
+
+            cardTitle: {
+                type: String
+            },
+
+            link: {
+                type: String
+            }
+        },
+
+        mounted() {
+            //
+        }
+    }
+</script>
