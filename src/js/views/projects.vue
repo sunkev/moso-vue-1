135040a034498c22b0d21404dfbbeeb3b10b1acc
Morten Sørensen <morten@moso.io>
2017-09-13 17:26:19 +0200

    Moved the small projects from Resume to Projects, and adjusted styling

--- src/js/views/projects.vue
+++ src/js/views/projects.vue
@@ -50,6 +50,38 @@
                 </div>
             </div>
         </div>
+
+        <div class="row resume">
+            <div class="md-12 xl-10 xl-offset-1">
+                <div class="row">
+                    <div class="sm-12">
+                        <h2 class="stuff-headline">I've also made stuff for</h2>
+                    </div>
+                </div>
+                <div class="row" v-masonry transition-duration="0.2s" item-selector=".lg-3">
+                    <div class="sm-12 md-4 lg-3 masonry-column" v-masonry-tile>
+                        <moso-project class="single" image="/images/assets/scratcher.svg" project-title="Scratcher" sub-title="Gamification platform">
+                            <span></span>
+                        </moso-project>
+                    </div>
+                    <div class="sm-12 md-4 lg-3 masonry-column" v-masonry-tile>
+                        <moso-project class="single" image="/images/assets/vestergaard.svg" project-title="Vestergaard Huse" sub-title="House construction">
+                            <span></span>
+                        </moso-project>
+                    </div>
+                    <div class="sm-12 md-4 lg-3 masonry-column" v-masonry-tile>
+                        <moso-project class="single" image="/images/assets/boligadmin.svg" project-title="Boligadmin" sub-title="Landlord/tenant administration system">
+                            <span></span>
+                        </moso-project>
+                    </div>
+                    <div class="sm-12 md-4 lg-3 masonry-column" v-masonry-tile>
+                        <moso-project class="single" image="/images/assets/rush.png" project-title="Rush Danmark" sub-title="Indoor trampoline park">
+                            <span></span>
+                        </moso-project>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
