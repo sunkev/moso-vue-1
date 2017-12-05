68fcbb2a9778682bcdbcb322ce1a81ed974ebb81
Morten Sørensen <morten@moso.io>
2017-12-05 23:22:58 +0100

    Fixed vue-masonry selectors now working with flexgrid-tailwind syntax

--- src/js/views/projects.vue
+++ src/js/views/projects.vue
@@ -2,7 +2,7 @@
     <div class="container:fluid">
         <div class="row portfolio">
             <div class="md:12 xl:10 xl:offset:1">
-                <div class="row" v-masonry transition-duration="0.2s" item-selector=".xl:6">
+                <div class="row" v-masonry transition-duration="0.2s" item-selector=".xl\:6">
                     <div class="sm:12 xl:6 masonry-column" v-masonry-tile v-for="project in projects" :key="project.id">
                         <moso-project :image="project.image" :project-title="project.title" :sub-title="project.subtitle" :link="project.link" :icon="project.icon">
                             <span v-html="project.content"></span>
@@ -19,7 +19,7 @@
                         <h2 class="stuff-headline">I've also made stuff for</h2>
                     </div>
                 </div>
-                <div class="row" v-masonry transition-duration="0.2s" item-selector=".lg:6">
+                <div class="row" v-masonry transition-duration="0.2s" item-selector=".lg\:6">
                     <div class="md:12 lg:6 masonry-column" v-masonry-tile v-for="project in smallprojects" :key="project.id">
                         <moso-project class="single" :image="project.image" :project-title="project.title" :sub-title="project.subtitle">
                             <span></span>
