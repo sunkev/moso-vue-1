32e2774caac3a3326085252cd993b902264065a3
Morten Sørensen <morten@moso.io>
2018-01-21 00:29:58 +0100

    Added moso-timeline

---
+++ components/moso-timeline/moso-timeline-item.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h3 class="title">{{ entry }}</h3>
+        <div class="information">
+            <span class="location" v-if="location">
+                <img src="/images/map-marker.svg" alt=""> {{ location }}
+            </span>
+            <span class="time">{{ time }}</span>
+        </div>
+        <slot></slot>
+    </li>
+</template>
+<script>
+    export default {
+        props: {
+            entry: {
+                type: String
+            },
+
+            location: {
+                type: String
+            },
+
+            time: {
+                type: String
+            }
+        },
+
+        mounted() {
+            //
+        }
+    }
+</script>
---
+++ components/moso-timeline/moso-timeline.vue
@@ -0,0 +1,12 @@
+<template>
+    <ul class="timeline">
+        <slot></slot>
+    </ul>
+</template>
+<script>
+    export default {
+        mounted() {
+            //
+        }
+    }
+</script>
