1f5947229c230bca871e8e3d3515fb2e31d5a522
Morten Sørensen <morten@moso.io>
2018-01-21 00:28:28 +0100

    Added moso-profile-picture

---
+++ components/moso-profile-picture/moso-profile-picture.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="profile-picture">
+        <div class="media">
+            <div class="image">
+                <img v-lazy-load="image" alt="Morten Sørensen">
+            </div>
+            <slot name="name"></slot>
+            <slot name="position"></slot>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            image: String
+        },
+
+        mounted() {
+
+        }
+    }
+</script>
