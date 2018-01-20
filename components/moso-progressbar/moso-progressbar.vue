c4829c8923869995571ad97ff3744250cafec17a
Morten Sørensen <morten@moso.io>
2018-01-21 00:28:53 +0100

    Added moso-progressbar

---
+++ components/moso-progressbar/moso-progressbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="progressbar-container">
+        <div class="progressbar-values">
+            <span>{{ name }}</span>
+            <span>{{ percentage }}%</span>
+        </div>
+        <div class="progressbar">
+            <span class="progress" :style="'width: ' + percentage + '%'">
+                <span class="thumb"></span>
+            </span>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            name: {
+                type: String
+            },
+
+            percentage: {
+                type: String
+            }
+
+        },
+
+        mounted() {
+            //
+        }
+    }
+</script>
