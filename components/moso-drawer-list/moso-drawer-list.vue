dfec9c237b18d6c5c6d2c771aa0e41398d0d495e
Morten Sørensen <morten@moso.io>
2018-01-21 00:27:36 +0100

    Added moso-drawer-list

---
+++ components/moso-drawer-list/moso-drawer-list.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="drawer-list">
+        <ul id="drawerMenu">
+            <slot></slot>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        mounted() {
+        },
+    }
+</script>
