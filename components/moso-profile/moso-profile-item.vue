6ff14027fe1adcf6063c24aad1feb67d28590b16
Morten Sørensen <morten@moso.io>
2018-01-21 00:28:04 +0100

    Added moso-profile

---
+++ components/moso-profile/moso-profile-item.vue
@@ -0,0 +1,38 @@
+<template>
+    <li>
+        <a :href="link" class="button icon-button" :class="linkClass" tabindex="-1" :title="title" rel="noopener">
+            <img :src="svg" alt="" v-if="svg">
+            <i class="mdi" :class="icon" v-if="icon"></i>
+            <nano-ripple></nano-ripple>
+        </a>
+    </li>
+</template>
+<script>
+    export default {
+        props: {
+            link: {
+                type: String
+            },
+
+            linkClass: {
+                type: String
+            },
+
+            icon: {
+                type: String
+            },
+
+            svg: {
+                type: String
+            },
+
+            title: {
+                type: String
+            }
+        },
+
+        mounted() {
+            //
+        }
+    }
+</script>
