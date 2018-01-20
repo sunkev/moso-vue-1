c9209e52082a91e541f560379f661936c687f716
Morten Sørensen <morten@moso.io>
2018-01-21 00:31:17 +0100

    Added version-badge

---
+++ components/version-badge/version-badge.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="version-badge">
+        <slot>{{ version }}</slot>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            version: String
+        },
+
+        mounted() {
+
+        }
+    }
+</script>
