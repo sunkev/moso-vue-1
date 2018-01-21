2bfab36ba95c6e870fc6a8f0fc42bb01063748c3
Morten Sørensen <morten@moso.io>
2018-01-21 02:07:03 +0100

    Added version-badge style

--- components/version-badge/version-badge.vue
+++ components/version-badge/version-badge.vue
@@ -4,13 +4,35 @@
     </div>
 </template>
 <script>
-    export default {
-        props: {
-            version: String
-        },
+export default {
+    props: {
+        version: String
+    },
 
-        mounted() {
+    mounted() {
 
-        }
     }
+}
 </script>
+<style lang="scss">
+@import '~/assets/sass/variables';
+
+.version-badge {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 20px;
+    margin: 1.125rem 1.5rem;
+    padding: .625rem;
+    background-color: rgba(0,0,0,.26);
+    border-radius: 2px;
+    font-size: .6875rem;
+    color: #fff;
+    line-height: 1;
+
+    @media (max-width: 767px) {
+        margin: 1.125rem 0 1.125rem 1.5rem;
+        padding: .3125rem
+    }
+}
+</style>
