7d580a9ffc8e635a212a72d4c0d0e94f65a8847f
Morten Sørensen <morten@moso.io>
2018-02-13 14:59:24 +0100

    Added favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,9 +24,24 @@ module.exports = {
             { name: 'mobile-web-app-capable', content: 'yes' },
             { name: 'apple-mobile-web-app-capable', content: 'yes' },
             { name: 'application-name', content: 'Morten Sørensen | moso.io' },
+            { name: 'msapplication-TileColor', content: '#263238' },
+            { name: 'msapplication-TileImage', content: '/images/static/favicon/ms-icon-144x144.png' },
             { name: 'theme-color', content: '#263238' }
         ],
         link: [
+            { rel: 'apple-touch-icon', sizes: '57x57', href: '/images/static/favicon/apple-icon-57x57.png' },
+            { rel: 'apple-touch-icon', sizes: '60x60', href: '/images/static/favicon/apple-icon-60x60.png' },
+            { rel: 'apple-touch-icon', sizes: '72x72', href: '/images/static/favicon/apple-icon-72x72.png' },
+            { rel: 'apple-touch-icon', sizes: '76x76', href: '/images/static/favicon/apple-icon-76x76.png' },
+            { rel: 'apple-touch-icon', sizes: '114x114', href: '/images/static/favicon/apple-icon-114x114.png' },
+            { rel: 'apple-touch-icon', sizes: '120x120', href: '/images/static/favicon/apple-icon-120x120.png' },
+            { rel: 'apple-touch-icon', sizes: '144x144', href: '/images/static/favicon/apple-icon-144x144.png' },
+            { rel: 'apple-touch-icon', sizes: '152x152', href: '/images/static/favicon/apple-icon-152x152.png' },
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/static/favicon/apple-icon-180x180.png' },
+            { rel: 'icon', type: 'image/png', sizes: '192x192',  hre: '/images/static/favicon/android-icon-192x192.png' },
+            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/images/static/favicon/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/images/static/favicon/favicon-96x96.png' },
+            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/images/static/favicon/favicon-16x16.png' },
             { rel: 'manifest', href: '/manifest.json' }
         ]
     },
