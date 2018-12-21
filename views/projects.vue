5cbed158b23d5c96915264637f24bb5c2d60bd9d
Kevin Sun <sunkev@brandeis.edu>
2018-12-21 17:26:05 -0500

    change projects

--- views/projects.vue
+++ views/projects.vue
@@ -46,24 +46,46 @@ export default {
     },
     data() {
         return {
-            projects: [],
-            smallprojects: []
+            projects: [
+                {
+                    title: "qualtrics",
+                    subtitle: "A ruby gem that wraps the Qualtrics REST API",
+                    // image: "/images/github.svg",
+                    url: "https://github.com/sunkev/qualtrics",
+                    icon: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" width=\"24\" viewBox=\"0 0 24 24\"><path d=\"m12 2a10 10 0 0 0 -10 10c0 4.42 2.87 8.17 6.84 9.5 0.5 0.08 0.66-0.23 0.66-0.5v-1.69c-2.77 0.6-3.36-1.34-3.36-1.34-0.46-1.16-1.11-1.47-1.11-1.47-0.91-0.62 0.07-0.6 0.07-0.6 1 0.07 1.53 1.03 1.53 1.03 0.87 1.52 2.34 1.07 2.91 0.83 0.09-0.65 0.35-1.09 0.63-1.34-2.22-0.25-4.55-1.11-4.55-4.92 0-1.11 0.38-2 1.03-2.71-0.1-0.25-0.45-1.29 0.1-2.64 0 0 0.84-0.27 2.75 1.02 0.79-0.22 1.65-0.33 2.5-0.33s1.71 0.11 2.5 0.33c1.91-1.29 2.75-1.02 2.75-1.02 0.55 1.35 0.2 2.39 0.1 2.64 0.65 0.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91 0.36 0.31 0.69 0.92 0.69 1.85v2.74c0 0.27 0.16 0.59 0.67 0.5 3.97-1.34 6.83-5.08 6.83-9.5a10 10 0 0 0 -10 -10z\"/></svg>",
+                    description: "A convenient wrapper for the Qualtrics REST API V2. Allows the a registered Qualtrics user to create panels, change recipients and sent surveys."
+                },
+                {
+                    title: "RecLetter",
+                    subtitle: "A blockchain recommendation letter service",
+                    // image: "/images/github.svg",
+                    url: "https://github.com/vmishra22/cscie599bc/blob/master/Block%20Chain%20Use%20Case.pdf",
+                    icon: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" width=\"24\" viewBox=\"0 0 24 24\"><path d=\"m12 2a10 10 0 0 0 -10 10c0 4.42 2.87 8.17 6.84 9.5 0.5 0.08 0.66-0.23 0.66-0.5v-1.69c-2.77 0.6-3.36-1.34-3.36-1.34-0.46-1.16-1.11-1.47-1.11-1.47-0.91-0.62 0.07-0.6 0.07-0.6 1 0.07 1.53 1.03 1.53 1.03 0.87 1.52 2.34 1.07 2.91 0.83 0.09-0.65 0.35-1.09 0.63-1.34-2.22-0.25-4.55-1.11-4.55-4.92 0-1.11 0.38-2 1.03-2.71-0.1-0.25-0.45-1.29 0.1-2.64 0 0 0.84-0.27 2.75 1.02 0.79-0.22 1.65-0.33 2.5-0.33s1.71 0.11 2.5 0.33c1.91-1.29 2.75-1.02 2.75-1.02 0.55 1.35 0.2 2.39 0.1 2.64 0.65 0.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91 0.36 0.31 0.69 0.92 0.69 1.85v2.74c0 0.27 0.16 0.59 0.67 0.5 3.97-1.34 6.83-5.08 6.83-9.5a10 10 0 0 0 -10 -10z\"/></svg>",
+                    description: "Master's degree capstone project. A recommendation letter webapp that leverages smart contracts on ethereum blockchain to add accountability to recommendation letter requests, and transfers between the school, student and recommender. Paper is pending."
+                },
+                {
+                    title: "On the Trail",
+                    subtitle: "App that proves safer trails through better communication",
+                    // image: "/images/github.svg",
+                    url: "https://github.com/sunkev/on_the_trail",
+                    icon: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" width=\"24\" viewBox=\"0 0 24 24\"><path d=\"m12 2a10 10 0 0 0 -10 10c0 4.42 2.87 8.17 6.84 9.5 0.5 0.08 0.66-0.23 0.66-0.5v-1.69c-2.77 0.6-3.36-1.34-3.36-1.34-0.46-1.16-1.11-1.47-1.11-1.47-0.91-0.62 0.07-0.6 0.07-0.6 1 0.07 1.53 1.03 1.53 1.03 0.87 1.52 2.34 1.07 2.91 0.83 0.09-0.65 0.35-1.09 0.63-1.34-2.22-0.25-4.55-1.11-4.55-4.92 0-1.11 0.38-2 1.03-2.71-0.1-0.25-0.45-1.29 0.1-2.64 0 0 0.84-0.27 2.75 1.02 0.79-0.22 1.65-0.33 2.5-0.33s1.71 0.11 2.5 0.33c1.91-1.29 2.75-1.02 2.75-1.02 0.55 1.35 0.2 2.39 0.1 2.64 0.65 0.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91 0.36 0.31 0.69 0.92 0.69 1.85v2.74c0 0.27 0.16 0.59 0.67 0.5 3.97-1.34 6.83-5.08 6.83-9.5a10 10 0 0 0 -10 -10z\"/></svg>",
+                    description: "A simple website that will allow users to create custom text and email notifications for their friends and loved ones to keep everyone informed. The result of 4 hours of extreme agile hacking."
+                },
+                {
+                    title: "S3 JS Uploader",
+                    subtitle: "Pure javascript multipart file uploader",
+                    // image: "/images/github.svg",
+                    url: "https://github.com/sunkev/s3-js-file-uploader",
+                    icon: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" width=\"24\" viewBox=\"0 0 24 24\"><path d=\"m12 2a10 10 0 0 0 -10 10c0 4.42 2.87 8.17 6.84 9.5 0.5 0.08 0.66-0.23 0.66-0.5v-1.69c-2.77 0.6-3.36-1.34-3.36-1.34-0.46-1.16-1.11-1.47-1.11-1.47-0.91-0.62 0.07-0.6 0.07-0.6 1 0.07 1.53 1.03 1.53 1.03 0.87 1.52 2.34 1.07 2.91 0.83 0.09-0.65 0.35-1.09 0.63-1.34-2.22-0.25-4.55-1.11-4.55-4.92 0-1.11 0.38-2 1.03-2.71-0.1-0.25-0.45-1.29 0.1-2.64 0 0 0.84-0.27 2.75 1.02 0.79-0.22 1.65-0.33 2.5-0.33s1.71 0.11 2.5 0.33c1.91-1.29 2.75-1.02 2.75-1.02 0.55 1.35 0.2 2.39 0.1 2.64 0.65 0.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91 0.36 0.31 0.69 0.92 0.69 1.85v2.74c0 0.27 0.16 0.59 0.67 0.5 3.97-1.34 6.83-5.08 6.83-9.5a10 10 0 0 0 -10 -10z\"/></svg>",
+                    description: "Modular JavaScript file upload using Amazon's mutlipart upload and the JavaScript file API to upload files up to 2TB client side"
+                }
+            ],
+            smallprojects: [{
+                title: "BioProjects",
+                subtitle: "New England Biolab's Internal Experiment Tracker",
+                image: "/images/NEB.png"
+            }]
         }
-    },
-    async asyncData({ req, params }) {
-        performance.mark('getProjects:start')
-        const [
-            { data: projects },
-            { data: smallprojects }
-        ] = await Promise.all([
-            axios.get('https://api.morten.is/projects'),
-            axios.get('https://api.morten.is/smallprojects')
-        ])
-        return {
-            projects,
-            smallprojects
-        }
-        performance.mark('getProjects:end')
     },
     mounted() {
         if (typeof this.$redrawVueMasonry === 'function') {
